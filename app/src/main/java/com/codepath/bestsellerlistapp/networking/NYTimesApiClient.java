c27ed185ef6f79fce0014f2db933038c084bb335
John Santelises <43687971+GamingDoge69@users.noreply.github.com>
2022-01-28 10:29:21 -0500

    API Key updated

--- app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.java
@@ -24,8 +24,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class NYTimesApiClient {
 
-    // TODO: Replace the below API key with your own generated key
-    private static final String API_KEY = "<YOUR-API-KEY-GOES-HERE>";
+    private static final String API_KEY = "thzBVoB6SRtxie6xGbUrJRMnHqEv6A2j";
     private NYTimesService nyTimesService;
 
     public NYTimesApiClient() {
